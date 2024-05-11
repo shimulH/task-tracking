1da52fb4237ff286c177c5ca40935ffae2233dad
shimul <20032077+shimulH@users.noreply.github.com>
2024-05-11 19:48:33 +0600

    feat: check queries

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { log } from '../log';
+import { db } from '../db/drizzle';
+import { Board } from '../db/schema';
+import * as schema from '../db/schema';
+import { drizzle } from 'drizzle-orm/neon-http';
+
 // import { auth, clerkClient } from '@clerk/nextjs/server';
 
 async function layout({ children }: { children: React.ReactNode }) {
+  log.info('creating user due to clerk webhook');
+  const res = await db.select().from(Board);
+  console.log('boards', res);
   return (
     <div className='flex justify-center py-24 caret-lime-500'>
       layout{children}
