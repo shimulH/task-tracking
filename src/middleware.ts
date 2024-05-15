c466d653b94a2cae3cac02ff4086bdd34fdcc3e9
shimul <20032077+shimulH@users.noreply.github.com>
2024-05-15 22:50:30 +0600

    fix: middlewire config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { rootRoute } from './routes';
 
 const isProtectedRoute = createRouteMatcher(['/boards(.*)']);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
+  const isRootRoute = req.nextUrl.pathname === rootRoute;
+
+  if (isRootRoute) {
+    return Response.redirect(new URL('/boards', req.nextUrl));
+  }
 });
 
 export const config = {
---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+/**
+ * The root route will be redirected to auth
+ * The root route is currently not used
+ * @type {string}
+ */
+export const rootRoute = '/';
